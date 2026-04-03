81b0b3c6503b146797cd0a090233de7cdd44042e
hetvideshani <hetvideshani@gmail.com>
2026-04-03 22:23:32 +0530

    compiler resolved

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>DevEchelons - A Perfect Coding Plateform</title>
+        <title>DevSkill Builder - A Perfect Coding Plateform</title>
         <link
           rel="stylesheet"
           type="text/css"
@@ -34,9 +34,9 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
           integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMdHusH44Y5RXQxuk8a6KLWW5ds2Zp/1vl5Qj0D"
         />
-        <link rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"></link>
-        <link rel="stylesheet" 
+        <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"></link>
+        <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </head>
 
